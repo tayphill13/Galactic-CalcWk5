edca51c918361557b5c87ea1adc8e8393ae98d8a
Taylor Phillips <taylorphillips133@gmail.com>
2020-06-26 08:10:28 -0700

    add title names to package.json and webapck.config

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'project-name',
+      title: 'galactic-calculator',
       template: './src/index.html',
       inject: 'body'
     })
