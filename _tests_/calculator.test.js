5e77cdd1ce8714c1e4f742b206ebf6b6ff99dcd9
Taylor Phillips <taylorphillips133@gmail.com>
2020-06-26 11:53:58 -0700

    shift my tests around to make sure that previous ones still pass.  ALSO showing a good fail for my latest method for at LifeExpectancy

--- _tests_/calculator.test.js
+++ _tests_/calculator.test.js
@@ -26,16 +26,21 @@ describe  ('UserAge', () => {
     samplePerson.jupiterYears();
     expect(samplePerson.jupiterAge).toEqual(2);
   });
+
+  test ('should subtract given age from projected life expectancy in Earth years and give remaining earth years.', () => {
+    expect(samplePerson.earthYearsRemain()).toEqual(47);
+  });
 });
 
 describe  ('UserAge', () => {
   let samplePerson = new UserAge(85);
 
-  // test ('should subtract given age from projected life expectancy in Earth years.', () => {
-  //   expect(samplePerson.earthYearsRemain()).toEqual(47);
-  // });
-
-  test('should calculate how many years passed life expectancy on Earth', () => {
+  test('should calculate how many years passed life expectancy on Earth.', () => {
+    samplePerson.age;
     expect(samplePerson.earthYearsRemain()).toEqual(5);
+  });
+
+  test("should calculate if you're at life expectancy on Earth.", () => {
+    expect(samplePerson.earthYearsRemain()).toEqual(0);
   });
 });
